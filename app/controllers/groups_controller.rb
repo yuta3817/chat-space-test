d68102c6997109486cd816fdd1f0f6d3ac67d139
yuta3817 <yuta2213817@gmail.com>
2020-08-11 17:50:41 +0900

    Revert "test"
    This reverts commit bee815ec4baa4d52f11a3054e2aadf4f9a83a361.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,7 @@ class GroupsController < ApplicationController
 
   def index
   end
-  
+
   def new
     @group = Group.new
     @group.users << current_user
--- db/migrate/20200805022230_devise_create_users.rb
+++ db/migrate/20200805022230_devise_create_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddDeviseToUsers < ActiveRecord::Migration[6.0]
+class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
@@ -34,15 +34,13 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.0]
       # t.datetime :locked_at
 
 
-      # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps null: false
+      t.timestamps null: false
     end
 
+    add_index :users, :name, unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
-    add_index :users, :name, unique: true
   end
-
 end
